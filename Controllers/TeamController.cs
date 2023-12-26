e884f7e2b42ad8b9fd901d37db0b2cd1c95233b6
Soumya Sutar <soumsutar9@gmail.com>
2023-12-26 21:07:05 +0530

    Soumya"
    git commit -mSoumya
    
    git commit -m'Soumya

---
+++ Controllers/TeamController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+        static List<Team> ListTeams = new List<Team>()
+        {
+            new Team(){TeamId=1,TeamName="India",NOWC=3},
+            new Team(){TeamId=2,TeamName="Australia",NOWC=4},
+            new Team(){TeamId=3,TeamName="West Indies",NOWC=2},
+            new Team(){TeamId=4,TeamName="England",NOWC=1}
+        };
+        
+        public ActionResult Index()
+        {
+            
+            return View(ListTeams);
+        }
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int NOWC { get; set; }
+
+    }
+}
